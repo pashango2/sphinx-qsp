b56b3974ca38a4cf32c432f583bb3e368fcff99b
pashango2 <toshi19770331@gmail.com>
2017-02-18 12:55:59 +0900

    add tests

--- sphinx_qsp/__init__.py
+++ sphinx_qsp/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-from .quickstart_plus import main
+from .quickstart_plus import main, __version__
 
-__version__ = "0.1"
